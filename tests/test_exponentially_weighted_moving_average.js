4c280e73a6d9ce3e6ebb40d633dd3f591e168606
Dan Erickson <derickson@yammer-inc.com>
2011-04-11 17:29:41 -0700

    Added test for ExponentiallyWeightedMovingAverage's

---
+++ tests/test_exponentially_weighted_moving_average.js
@@ -0,0 +1,20 @@
+var ExponentiallyWeightedMovingAverage = require('../stats/exponentially_weighted_moving_average');
+var exponentially_weighted_moving_average = new ExponentiallyWeightedMovingAverage((1 - Math.exp(-5/60)));
+
+console.log("\nTesting ExponentiallyWeightedMovingAverage\n");
+console.log("Sending updates every 100 milliseconds for 30 seconds.");
+console.log("Ticking every second.\n");
+
+var tickInterval = setInterval(function(){
+  exponentially_weighted_moving_average.tick();
+  process.stdout.write(".")
+}, 1000);
+var updateInterval = setInterval(function(){
+  exponentially_weighted_moving_average.update();
+}, 100);
+setTimeout(function(){
+  clearInterval(tickInterval);
+  clearInterval(updateInterval);
+  console.log("\n\nExpected Average: 10");
+  console.log("Exponentially Weighted Moving Average: "+exponentially_weighted_moving_average.rate());
+}, 30000);
