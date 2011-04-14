3c9184ba2498947e313904e0c5b21a0cf727abb7
Dan Erickson <derickson@yammer-inc.com>
2011-04-14 15:40:19 -0700

    Cleaned up the tests for the stats objects, made test_all.js much more flexible

--- tests/test_all.js
+++ tests/test_all.js
@@ -1,13 +1,21 @@
-var tests = [
-  require('./test_uniform_sample')
-, require('./test_exponentially_decaying_sample')
-, require('./test_exponentially_weighted_moving_average')
-]
-
-for (var i in tests) {
-  if (typeof tests[i] == 'function' && typeof tests[i+1] == 'function') {
-    tests[i](tests[i+1]());
-  } else if (typeof tests[i] == 'function') {
-    tests[i]();
-  }
+/*
+ * Returns a function that is the composition of a list of functions, 
+ * each consuming the return value of the function that follows.
+ */
+var slice = Array.prototype.slice;
+var compose = function() {
+  var funcs = slice.call(arguments);
+  return function() {
+    var args = slice.call(arguments);
+    for (var i=funcs.length-1; i >= 0; i--) {
+      args = [funcs[i].apply(this, args)];
+    }
+    return args[0];
+  };
 };
+
+compose(
+  require('./test_exponentially_weighted_moving_average')
+, require('./test_exponentially_decaying_sample')
+, require('./test_uniform_sample')
+)();
