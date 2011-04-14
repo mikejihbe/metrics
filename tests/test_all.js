d6316867d60d8718faf52e96c9328684165eed37
Dan Erickson <derickson@yammer-inc.com>
2011-04-14 14:50:21 -0700

    Cleaned up the tests for the stats objects, made test_all.js much more flexible

--- tests/test_all.js
+++ tests/test_all.js
@@ -1,8 +1,13 @@
-var testUniformSample = require('./test_uniform_sample');
-var testExponentiallyWeightedMovingAverage = require('./test_exponentially_weighted_moving_average');
+var tests = [
+  require('./test_uniform_sample')
+, require('./test_exponentially_decaying_sample')
+, require('./test_exponentially_weighted_moving_average')
+]
 
-testExponentiallyWeightedMovingAverage(function(){
-  testUniformSample(function(){
-    // Next test goes here.
-  });
-});
+for (var i in tests) {
+  if (typeof tests[i] == 'function' && typeof tests[i+1] == 'function') {
+    tests[i](tests[i+1]());
+  } else if (typeof tests[i] == 'function') {
+    tests[i]();
+  }
+};
--- tests/test_exponentially_decaying_sample.js
+++ tests/test_exponentially_decaying_sample.js
@@ -1,34 +1,45 @@
 var ExponentiallyDecayingSample = require('../stats/exponentially_decaying_sample.js')
   , eds = new ExponentiallyDecayingSample(1000, 0.015);
 
-eds.clear();
+var test = function(callback) {
+  eds.clear();
 
-var time = (new Date()).getTime()
-  , interval = 60 * 60 * 1000 / 100;
-for(var i = 0; i < 100; i++) {
-  for(var j = 0; j < 100; j++) {
-    eds.update(i, time + i * interval);
-  }
-}
-
-function printSample(eds) {
-  var valueCounts = {}
-    , values = eds.getValues();
-  
-  for(var i = 0; i < eds.size(); i++) {
-    if (valueCounts[values[i].val]) {
-      valueCounts[values[i].val]++;
-    } else {
-      valueCounts[values[i].val] = 1;
+  var time = (new Date()).getTime()
+    , interval = 60 * 60 * 1000 / 100;
+  for(var i = 0; i < 100; i++) {
+    for(var j = 0; j < 100; j++) {
+      eds.update(i, time + i * interval);
     }
   }
-  return valueCounts;
+
+  function printSample(eds) {
+    var valueCounts = {}
+      , values = eds.getValues();
+  
+    for(var i = 0; i < eds.size(); i++) {
+      if (valueCounts[values[i].val]) {
+        valueCounts[values[i].val]++;
+      } else {
+        valueCounts[values[i].val] = 1;
+      }
+    }
+    return valueCounts;
+  }
+
+  console.log("This is an exponential distribution:");
+  console.log(printSample(eds));
+
+  eds.rescale(time + 100*interval);
+
+  console.log("This is after rescaling");
+  console.log(eds.getValues());
+  if (typeof callback == 'function') {
+    callback();
+  }
 }
 
-console.log("This is an exponential distribution:");
-console.log(printSample(eds));
-
-eds.rescale(time + 100*interval);
-
-console.log("This is after rescaling");
-console.log(eds.getValues());
+if (module.parent) {
+  module.exports = test;
+} else {
+  test();
+}
