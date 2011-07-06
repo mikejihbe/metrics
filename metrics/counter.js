7af921baa7b0008ea8068d0c40848ae8d6190501
Michael S. Fischer <michael.fischer@rackspace.com>
2011-07-05 20:11:29 -0700

    Additional checking for counters.
    This patch adds some additional checking for counters:
    
    * Wrap counters around at 2^32.  Most graphing software (RRDtool etc.)
      can deal with this properly.  Unfortunately wrapping at 2^64 isn't
      possible because JS has a maximum integer resolution of 2^53.
    
    * Don't allow counters to be decremented below 0.

--- metrics/counter.js
+++ metrics/counter.js
@@ -1,6 +1,12 @@
 /*
 *  A simple counter object
 */
+
+/* JavaScript uses double-precision FP for all numeric types.  
+ * Perhaps someday we'll have native 64-bit integers that can safely be
+ * transported via JSON without additional code, but not today. */
+var MAX_COUNTER_VALUE = Math.pow(2, 32); // 4294967296
+
 var Counter = module.exports = function Counter() {
   this.count = 0;
   this.type = 'counter';
@@ -8,12 +14,18 @@ var Counter = module.exports = function Counter() {
 
 Counter.prototype.inc = function(val) {
   if (!val) { val = 1; }
-  this.count += val;
+  if (this.count === MAX_COUNTER_VALUE) {
+    this.count = 0;
+  } else {
+    this.count += val;
+  }
 }
 
 Counter.prototype.dec = function(val) {
   if (!val) { val = 1; }
-  this.count -= val;
+  if (this.count > 0) {
+    this.count -= val;
+  }
 }
 
 Counter.prototype.clear = function() {
