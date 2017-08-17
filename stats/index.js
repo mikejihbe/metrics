792d3f7e38beb92d2c5e614f98e6ec10e1628a9b
Serbay Arslanhan <serbay.arslanhan@zalando.de>
2017-08-17 09:53:22 +0200

    Export modules under ./stats directory and add type definitions

---
+++ stats/index.js
@@ -0,0 +1,4 @@
+exports.EWMA = require('./exponentially_weighted_moving_average');
+exports.ExponentiallyDecayingSample = require('./exponentially_decaying_sample');
+exports.Sample = require('./sample');
+exports.UniformSample = require('./uniform_sample');
